e0adaa5869fbb31d90543528a8f7e789f4f7346b
Joseph Gan <josephgan@live.com.sg>
2019-05-10 23:21:34 +0800

    Update: data set

--- src/data.js
+++ src/data.js
@@ -1,8 +1,11 @@
 const data = {
     data: [
-      // {id: 1, text: 'Task #1', start_date: '17-04-2019', duration: 8, progress: 0.6, priority: 'high'},
-      // {id: 2, text: 'Task #2', start_date: '18-04-2019', duration: 3, progress: 0.4, priority: 'normal'},
-      // {id: 3, text: 'Task #3', start_date: '20-04-2019', duration: 3, progress: 0.4, priority: 'low'}
+      // {id: 1, text: 'Task #1', start_date: '10-05-2019', duration: 8, progress: 0.6, priority: 'high'},
+      // {id: 2, text: 'Task #2', start_date: '11-05-2019', duration: 3, progress: 0.4, priority: 'normal'},
+      // {id: 3, text: 'Task #3', start_date: '12-05-2019', duration: 3, progress: 0.4, priority: 'low'},
+      // {id: 4, text: 'Task #4', start_date: '13-05-2019', duration: 3, progress: 0.4, priority: 'low'},
+      // {id: 5, text: 'Task #5', start_date: '14-05-2019', duration: 3, progress: 0.4, priority: 'low'},
+      // {id: 6, text: 'Task #6', start_date: '15-05-2019', duration: 3, progress: 0.4, priority: 'low'}
     ],
 };
 
